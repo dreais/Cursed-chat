7c8d1a0eca8cd35157de7d67cb14c15ea700a626
dreais <valentin.lattay@gmail.com>
2019-10-24 00:30:03 +0200

    initializing WSA, added log output

---
+++ Headers/core_server.h
@@ -0,0 +1,11 @@
+//
+// Created by Valentin on 10/24/2019.
+//
+
+#ifndef CURSED_CHAT_CORE_SERVER_H
+#define CURSED_CHAT_CORE_SERVER_H
+
+#include <winsock.h>
+#include "log_file.h"
+
+#endif //CURSED_CHAT_CORE_SERVER_H
--- Sources/Server/server.c
+++ Sources/Server/server.c
@@ -2,3 +2,26 @@
 // Created by Valentin on 10/24/2019.
 //
 
+#include "../../Headers/core_server.h"
+
+int winsock_initialize(void)
+{
+	WSADATA wsa_data;
+	int result;
+
+	result = WSAStartup(MAKEWORD(2, 2), &wsa_data);
+	if (result != NO_ERROR) {
+		output_logs_str(PREFIX_ERROR, "WSA couldn't initialize. Exiting with error code %d\n", result);
+		exit(result);
+	}
+	return result;
+}
+
+int server_socket_initialize(void)
+{
+	SOCKET server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	int optval = 0;
+
+	setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, (char *) &optval, sizeof(int));
+	return 0;
+}
